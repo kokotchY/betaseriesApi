b95f4e1d9f16b6ccb475573741c1e9ebd4cf1676
Sébastien Canart <kokotchy@gmail.com>
2010-10-18 20:34:53 +0000

    Getter and setter
    git-svn-id: http://localhost/svn/betaserieApi/trunk@11 9ded2195-1b7e-45c4-b482-60075756efdb

--- betaSeriesAPI/src/main/java/com/kokotchy/betaSeriesAPI/model/Notification.java
+++ betaSeriesAPI/src/main/java/com/kokotchy/betaSeriesAPI/model/Notification.java
@@ -25,4 +25,64 @@ public class Notification {
 	 * 
 	 */
 	private boolean seen;
+
+	/**
+	 * @return the date
+	 */
+	public int getDate() {
+		return date;
+	}
+
+	/**
+	 * @return the html
+	 */
+	public String getHtml() {
+		return html;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * @return the seen
+	 */
+	public boolean isSeen() {
+		return seen;
+	}
+
+	/**
+	 * @param date
+	 *            the date to set
+	 */
+	public void setDate(int date) {
+		this.date = date;
+	}
+
+	/**
+	 * @param html
+	 *            the html to set
+	 */
+	public void setHtml(String html) {
+		this.html = html;
+	}
+
+	/**
+	 * @param id
+	 *            the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/**
+	 * @param seen
+	 *            the seen to set
+	 */
+	public void setSeen(boolean seen) {
+		this.seen = seen;
+	}
 }
