1471593dfa94cd2dd77be6dd3f371e6f4a45dcf7
Sébastien Canart <kokotchy@gmail.com>
2010-11-11 14:38:05 +0000

    retrieve result
    git-svn-id: http://localhost/svn/betaserieApi/trunk@48 9ded2195-1b7e-45c4-b482-60075756efdb

--- betaSeriesAPI/src/test/resources/retrieve.sh
+++ betaSeriesAPI/src/test/resources/retrieve.sh
@@ -2,8 +2,10 @@
 
 for i in `cat list`
 do
-    curl -d `echo $i | cut -d '|' -f 2` http://api.betaseries.com/`echo $i \
-        | cut -d '|' -f 1`.xml > `echo $i | cut -d '|' -f 1 | sed -e 's@/@\.@g'`.xml;
-    curl -d `echo $i | cut -d '|' -f 2` http://api.betaseries.com/`echo $i \
-    | cut -d '|' -f 1`.json > `echo $i | cut -d '|' -f 1 | sed -e 's@/@\.@g'`.json
+    parameters=`echo $i | cut -d '|' -f 2`
+    action=`echo $i | cut -d '|' -f 1`
+    actionfile=`echo $action | sed -e 's@/@\.@g'`
+    urlfile=http://api.betaseries.com/$action
+    curl -d $parameters $urlfile.xml > $actionfile.xml
+    curl -d $parameters $urlfile.json > $actionfile.json
 done
