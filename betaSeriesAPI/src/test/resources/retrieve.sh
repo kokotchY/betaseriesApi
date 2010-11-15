5d1a420024f0432190c09a119806933deaa4bdd6
Sébastien Canart <kokotchy@gmail.com>
2010-11-15 16:53:19 +0100

    Add params to file

--- betaSeriesAPI/src/test/resources/retrieve.sh
+++ betaSeriesAPI/src/test/resources/retrieve.sh
@@ -2,10 +2,24 @@
 
 for i in `cat list`
 do
+    oldIfs=$IFS
     parameters=`echo $i | cut -d '|' -f 2`
+    IFS="&"
+    sortedParam=""
+    first="true"
+    for j in $parameters
+    do
+        if [ $first = "true" ]
+        then
+            sortedParam="$j"
+        else
+            sortedParam="${sortedParam}_$j"
+        fi
+    done
+    IFS=$oldIfs
     action=`echo $i | cut -d '|' -f 1`
     actionfile=`echo $action | sed -e 's@/@\.@g'`
     urlfile=http://api.betaseries.com/$action
-    curl -d $parameters $urlfile.xml > $actionfile.xml
-    curl -d $parameters $urlfile.json > $actionfile.json
+    curl -d $parameters $urlfile.xml > $actionfile-$sortedParam.xml
+    curl -d $parameters $urlfile.json > $actionfile-$sortedParam.json
 done
