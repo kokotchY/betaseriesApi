639391167e949c9a788b023cda7e779d64d2af02
Sébastien Canart <kokotchy@gmail.com>
2010-12-19 16:10:27 +0100

    update test files

--- betaSeriesAPI/src/test/java/com/kokotchy/betaSeriesAPI/TestMembers.java
+++ betaSeriesAPI/src/test/java/com/kokotchy/betaSeriesAPI/TestMembers.java
@@ -44,29 +44,6 @@ public class TestMembers extends TestCase {
 	 */
 	private String key;
 
-	@Override
-	protected void setUp() throws Exception {
-		super.setUp();
-		String userDir = System.getProperty("user.dir");
-		key = Utils.getApiKey(userDir);
-		File credentialsFile = new File(userDir,
-				"src/test/resources/credentials.txt");
-		String[] credentials = Utils.loadCredentials(credentialsFile);
-		login = credentials[0];
-		password = credentials[1];
-		token = credentials[2];
-		com.kokotchy.betaSeriesAPI.api.jsonImpl.BetaSerieApi.setApiKey(key);
-		com.kokotchy.betaSeriesAPI.api.xmlImpl.BetaSerieApi.setApiKey(key);
-		membersJson = com.kokotchy.betaSeriesAPI.api.jsonImpl.BetaSerieApi
-				.getMembers();
-		membersXml = com.kokotchy.betaSeriesAPI.api.xmlImpl.BetaSerieApi
-				.getMembers();
-		UtilsJson.setDebug(true);
-		UtilsJson.setDebugPath(userDir + "/src/test/resources/json/");
-		UtilsXml.setDebug(true);
-		UtilsXml.setDebugPath(userDir + "/src/test/resources/xml/");
-	}
-
 	/**
 	 * Test authentication of the user
 	 */
@@ -163,5 +140,28 @@ public class TestMembers extends TestCase {
 	 */
 	public void testSignup() {
 		fail("Not implemented");
+	}
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		String userDir = System.getProperty("user.dir");
+		key = Utils.getApiKey(userDir);
+		File credentialsFile = new File(userDir,
+				"src/test/resources/credentials.txt");
+		String[] credentials = Utils.loadCredentials(credentialsFile);
+		login = credentials[0];
+		password = credentials[1];
+		token = credentials[2];
+		com.kokotchy.betaSeriesAPI.api.jsonImpl.BetaSerieApi.setApiKey(key);
+		com.kokotchy.betaSeriesAPI.api.xmlImpl.BetaSerieApi.setApiKey(key);
+		membersJson = com.kokotchy.betaSeriesAPI.api.jsonImpl.BetaSerieApi
+				.getMembers();
+		membersXml = com.kokotchy.betaSeriesAPI.api.xmlImpl.BetaSerieApi
+				.getMembers();
+		UtilsJson.setDebug(true);
+		UtilsJson.setDebugPath(userDir + "/src/test/resources/json/");
+		UtilsXml.setDebug(true);
+		UtilsXml.setDebugPath(userDir + "/src/test/resources/xml/");
 	}
 }
